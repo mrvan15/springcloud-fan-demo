bb60b54b46b94c17fc44edea5459dd4757e8a88b
mrvan15 <woshifanguannan@163.com>
2019-06-06 00:08:26 +0800

    add client

---
+++ spring-cloud-config-client-demo/src/main/java/com/fan/springcloudconfigclientdemo/SpringCloudConfigClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fan.springcloudconfigclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientDemoApplication.class, args);
+	}
+
+}
